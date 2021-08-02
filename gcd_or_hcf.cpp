e400b5fc9a020779243ef2b52fbecc758fb45667
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-08-02 09:03:05 +0530

    Create gcd_or_hcf.cpp

---
+++ gcd_or_hcf.cpp
@@ -0,0 +1,49 @@
+--------------------------
+find the hcf of two number 
+--------------------------
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int hcf(int a,int b)
+{
+    //divides to 0
+    if(a==0)
+    {
+        return b;
+    }
+    if(b==0)
+    {
+        return a;
+    }
+    
+    //both are equal
+    if(a==b)
+    {
+        return a;
+    }
+    
+    if(a>b)
+    {
+        return hcf(a-b,b);
+    }
+    return hcf(a,b-a);
+    
+}
+ 
+int main ()
+{
+   
+  //highest common factor of two no 
+  int a,b;
+  cout<<"Enter a :"<<endl;
+  cin>>a;
+  cout<<"Enter b :"<<endl;
+  cin>>b;
+  
+  cout<<hcf(a,b)<<endl;
+   
+   
+   
+   
+    return 0;
+}
